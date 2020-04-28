a6912ed46f656b8de3c91493067fc65c4a71d0e5
Flo <florian.kroeber@planblick.com>
2020-04-28 16:52:23 +0200

    Calculate points of all players including self

--- code/website/static/js/blackjack.js
+++ code/website/static/js/blackjack.js
@@ -1,7 +1,9 @@
 const gameData = {}
 gameData["startCardsAmount"] = 2
 gameData["colors"] = ["C", "D", "H","S"]
-gameData["values"] = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+gameData["cardnames"] = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+gameData["cardValueMapping"] = {"2": 2, "3":3, "4":4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9,
+                                "10": 10, "J": 10, "Q": 10, "K": 10, "A": 11}
 gameData["playerList"] = ["player_1", "bank"]
 gameData["playerData"] = {}
 for(let player of gameData["playerList"]) {
@@ -28,7 +30,7 @@ function drawCard(player, reveal_card=true, amount=1) {
         i += 1
         let new_image_element = document.createElement("img");
         let colors = gameData["colors"]
-        let values = gameData["values"]
+        let values = gameData["cardnames"]
         let color = colors[Math.floor(Math.random() * colors.length)];
         let value = values[Math.floor(Math.random() * values.length)];
         let card = value + color
@@ -50,4 +52,22 @@ function drawCard(player, reveal_card=true, amount=1) {
 
 function noMoreCards(player) {
     document.getElementById(player + "_controls").style.display="none"
+    bankTurn()
+}
+
+function bankTurn() {
+    /* 1) Calculate points of all players including self */
+    let points = {}
+    for(let player of gameData["playerList"]) {
+        console.log("Player:", player, "Cards:", gameData["playerData"][player]["cards"])
+        points[player] = 0
+        for(let card of gameData["playerData"][player]["cards"]) {
+            let valuePart = card.slice(0, -1);
+            let value = gameData["cardValueMapping"][valuePart]
+            points[player] += value
+        }
+    }
+    /* 2) Make decision whether to draw another cards or finish game */
+    /* 3.1) Draw card and start again at 1) */
+    /* 3.2) Call final function */
 }
