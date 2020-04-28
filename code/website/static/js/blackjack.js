ad8912b4e47843b2fde5e022e3f78aaa55274985
Flo <florian.kroeber@planblick.com>
2020-04-28 15:44:35 +0200

    draw random cards now

--- code/website/static/js/blackjack.js
+++ code/website/static/js/blackjack.js
@@ -1,7 +1,13 @@
 function draw_cards() {
     let image_elements = document.getElementById("cards_player_1").childNodes
+    let colors = ["C", "D", "H","S"]
+    let values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
     for(image of image_elements) {
-        image.src="./images/cards/2C.png"
+        let color = colors[Math.floor(Math.random() * colors.length)];
+        let value = values[Math.floor(Math.random() * values.length)];
+        let card = value + color
+        let image_name = card + ".png"
+        image.src="./images/cards/" + image_name
     }
 }
