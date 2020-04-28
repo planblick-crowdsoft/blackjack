c4c2f8d0e5d6383d85f32adc7ff1e9c53327f70e
Flo <florian.kroeber@planblick.com>
2020-04-28 16:36:29 +0200

    Midway-refactoring done

--- code/website/static/js/blackjack.js
+++ code/website/static/js/blackjack.js
@@ -1,60 +1,51 @@
 const gameData = {}
+gameData["startCardsAmount"] = 2
 gameData["colors"] = ["C", "D", "H","S"]
 gameData["values"] = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 gameData["playerList"] = ["player_1", "bank"]
-gameData["playerdata"] = {}
+gameData["playerData"] = {}
 for(let player of gameData["playerList"]) {
-    gameData["playerdata"][player] = {"cards":[]}
+    gameData["playerData"][player] = {"cards":[]}
 }
 
 function newGame() {
     let players=gameData["playerList"]
     for(let player of players) {
-        let reveal_cards = true
+        document.getElementById("cards_" + player).innerHTML=""
         if(player == "bank") {
-            reveal_cards = false
+            drawCard(player, false, gameData["startCardsAmount"])
+        } else {
+            document.getElementById(player + "_controls").style.display="block"
+            drawCard(player, true, gameData["startCardsAmount"])
         }
-        gameData["playerdata"][player]["cards"] = drawCards(player, reveal_cards)
+
     }
 }
 
-
-function drawCards(person, reveal_cards=true) {
-    let image_elements = document.getElementById("cards_" + person).getElementsByTagName('img')
-    let colors = gameData["colors"]
-    let values = gameData["values"]
-    let cards = []
-    for(let image of image_elements) {
+function drawCard(player, reveal_card=true, amount=1) {
+    let i=0
+    while(i < amount) {
+        i += 1
+        let new_image_element = document.createElement("img");
+        let colors = gameData["colors"]
+        let values = gameData["values"]
         let color = colors[Math.floor(Math.random() * colors.length)];
         let value = values[Math.floor(Math.random() * values.length)];
         let card = value + color
-        cards.push(card)
-        if(reveal_cards) {
-            let image_name = card + ".png"
-            image.src = "./images/cards/" + image_name
+        let image_name = "gray_back.png"
+        if (reveal_card) {
+            image_name = card + ".png"
         }
-    }
-    return cards
-}
 
-function drawCard(person, reveal_card=true) {
-    let new_image_element = document.createElement("img");
-    let colors = gameData["colors"]
-    let values = gameData["values"]
-    let color = colors[Math.floor(Math.random() * colors.length)];
-    let value = values[Math.floor(Math.random() * values.length)];
-    let card = value + color
-
-    if(reveal_card) {
-        let image_name = card + ".png"
         new_image_element.src = "./images/cards/" + image_name
         new_image_element.className = "card_image"
-        let persons_card_node = document.getElementById("cards_" + person)
-        persons_card_node.appendChild(new_image_element)
-        console.log(persons_card_node)
+        let players_card_node = document.getElementById("cards_" + player)
+        players_card_node.appendChild(new_image_element)
+        console.log(players_card_node)
+
+        gameData["playerData"][player]["cards"].push(card)
     }
 
-    return card
 }
 
 function noMoreCards(player) {
